424a41ca0a48c4fa333813b25d382e85cfad5ca0
VIpiin J S <vipin.john@gmail.com>
2020-07-28 21:09:11 +0530

    Create ad.js

---
+++ cdn/ad.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+  // To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ad.js
+  
+  document.body.style.backgroundColor = "black";
+  debugger;
+  console.log("Full window object", window);
+  console.log("Parent window", window.parent);
+});
+
+function loadAd()
+{
+  
+}
